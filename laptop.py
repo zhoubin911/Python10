64d27bb56e7b5f4566b4661ad9a5fe1c09efff35
zhoubin911 <zhobinghahvarehchi@gmail.com>
2025-10-01 22:13:19 +0330

    Delete laptop.py

--- laptop.py
+++
@@ -1,8 +0,0 @@
-from tamrin.electrical import Electrical
-
-class Laptop(Electrical):
-    def __init__(self, name, price, voltage, cpu, ram):
-        super().__init__(name, price, voltage)
-        self.cpu = cpu
-        self.ram = ram
-
