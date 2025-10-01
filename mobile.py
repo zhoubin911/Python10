623cada3301309d1e857de3605cdc85c2737279d
zhoubin911 <zhobinghahvarehchi@gmail.com>
2025-10-01 22:13:52 +0330

    Delete mobile.py

--- mobile.py
+++
@@ -1,9 +0,0 @@
-from tamrin.electrical import Electrical
-
-class Mobile(Electrical):
-    def __init__(self, name, price, voltage, screen_size, brand):
-        super().__init__(name, price, voltage)
-        self.screen_size = screen_size
-        self.brand = brand
-
-
