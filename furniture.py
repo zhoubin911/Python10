889c5b554488045ca168926854e92ce9c80ead2d
zhoubin911 <zhobinghahvarehchi@gmail.com>
2025-10-01 22:12:57 +0330

    Delete furniture.py

--- furniture.py
+++
@@ -1,7 +0,0 @@
-from tamrin.non_electrical import Non_Electrical
-
-class Furniture(Non_Electrical):
-    def __init__(self, name, price, weight, capacity):
-        super().__init__(name, price, weight)
-        self.capacity = capacity
-
